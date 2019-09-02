37cb7bde395063fb2605deef0bdd253a661e9b53
Nathan Pieper <nrpieper@gmail.com>
2019-09-02 10:01:07 -0500

    Updated TOC for Wow version 8.2

--- Misspelled.lua
+++ Misspelled.lua
@@ -95,7 +95,7 @@ Misspelled = LibStub("AceAddon-3.0"):NewAddon("Misspelled", "AceEvent-3.0", "Ace
 
 local Misspelled = _G.Misspelled
 
-Misspelled.Version = "Misspelled v1.6.3"
+Misspelled.Version = "Misspelled v1.7.3"
 
 local AceGUI = LibStub("AceGUI-3.0")
 local L = LibStub("AceLocale-3.0"):GetLocale("Misspelled", true)
